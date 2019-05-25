5514d539809610ae4ace34d944b5a1b127016a0f
Vasia Leleka <vasialeleka@ukr.net>
2019-05-25 10:48:00 -0700

    add tableView

--- Todo/Todo/ViewController.swift
+++ Todo/Todo/ViewController.swift
@@ -8,9 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var data: [String] = ["first","second"]
 
-    override func viewDidLoad() {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cellid",for : indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     return data.count
+    }
+    
+   override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
