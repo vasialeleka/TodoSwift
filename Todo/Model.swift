cdbf1f2c9d90cba85a12f7e17f672777470064f6
Vasia Leleka <vasialeleka@ukr.net>
2019-05-26 06:46:08 -0700

    add get and set for list

--- Todo/Model.swift
+++ Todo/Model.swift
@@ -1,32 +1,32 @@
 
 import Foundation
 
-var TodoList: [[String:Any]] = [["Name":"first","isCompleted": false ],["Name":"second","isCompleted": true ]]
+var TodoList: [[String:Any]] {
+    set {
+        UserDefaults.standard.set(newValue, forKey: "TodoDataKey")
+        UserDefaults.standard.synchronize()
+    }
+    get{
+        if let array = UserDefaults.standard.array(forKey: "TodoDataKey") as?[[String : Any]] {
+            return  array
+        }else{
+            return []
+        }
+    }
+}
 
 func addItem(todoItem: String, isCompleted:Bool = false){
     TodoList.append(["Name":todoItem,"isCompleted":isCompleted])
-    saveData()
+
 }
 func removeItem(at itemIndex: Int){
     TodoList.remove(at: itemIndex)
-    saveData()
 }
 
 func changeState(at item:Int)->Bool{
     TodoList[item]["isCompleted"] = !(TodoList[item]["isCompleted"] as! Bool)
-    saveData()
     return TodoList[item]["isCompleted"] as! Bool
     
 }
-func saveData(){
-    UserDefaults.standard.set(TodoList, forKey: "TodoDataKey")
-    UserDefaults.standard.synchronize()
-}
-func loadData(){
-    if let array = UserDefaults.standard.array(forKey: "TodoDataKey") as?[[String : Any]] {
-        TodoList = array
-    }else{
-        TodoList = []
-    }
-}
+
 
